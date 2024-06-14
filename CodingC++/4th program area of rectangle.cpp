a871426820269c13eb52fb1a25f29c1010bd554f
ZH-19 <famehyder9999@gmail.com>
2024-06-14 11:22:54 +0500

    C++

---
+++ CodingC++/4th program area of rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float l;
+float w;
+char area;
+
+cout<<"Enter the length = ";
+cin>>l;
+
+cout<<"Enter the width = ";
+cin>>w;
+
+cout<<"The area of rectangle = "<<l*w;
+
+return 0 ;
+}
