e76e6591c1a3d17f35a76eca1ecdf70c19cc19d5
ZH-19 <famehyder9999@gmail.com>
2024-06-16 15:59:13 +0500

    C++

---
+++ CodingC++/8th program even odd number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{int a,b;
+cout<<"Enter the number = ";
+cin>>a;
+
+if(b==a%2)
+cout<<"The number is even";
+
+else
+cout<<"The number is odd";
+
+return 0 ;
+}
