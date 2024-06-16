dc0ff02d51631b6bc27bff70ea4924c17c74f897
ZH-19 <famehyder9999@gmail.com>
2024-06-16 15:56:17 +0500

    C++

---
+++ CodingC++/7th program sum and avg.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,sum,avg;
+
+cout<<"Enter first number = ";
+cin>>a;
+
+cout<<"Enter second number = ";
+cin>>b;
+
+sum = a+b;
+cout<<endl<<endl<<"sum of these numbers = "<<sum<<endl<<endl;
+
+avg = sum/2;
+
+cout<<"Avg of these numbers = "<<avg;
+return 0;
+}
